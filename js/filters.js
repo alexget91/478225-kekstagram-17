d55fa594c9baf369f28b806623d6e714b23361ea
Alex <alex.get91@gmail.com>
2019-07-03 16:03:49 +0300

    Фильтры в галерее изображений

---
+++ js/filters.js
@@ -0,0 +1,49 @@
+// Фильтры галереи фотографий
+'use strict';
+
+(function () {
+  var FILTER_ACTIVE_CLASS = 'img-filters__button--active';
+  var NEW_PHOTOS_COUNT = 10;
+
+  var filters = document.querySelector('.img-filters');
+
+  var compareRandom = function () {
+    return Math.random() - 0.5;
+  };
+
+  var compareComments = function (a, b) {
+    return b.comments.length - a.comments.length;
+  };
+
+  var setActiveButton = function (target) {
+    filters.querySelector('.' + FILTER_ACTIVE_CLASS).classList.remove(FILTER_ACTIVE_CLASS);
+    target.classList.add(FILTER_ACTIVE_CLASS);
+  };
+
+  var applyFilter = window.debounce(function (arrPhotos, filterId) {
+    var newPhotos = arrPhotos;
+
+    switch (filterId) {
+      case 'filter-new':
+        newPhotos = arrPhotos.slice().sort(compareRandom).slice(0, NEW_PHOTOS_COUNT);
+        break;
+      case 'filter-discussed':
+        newPhotos = arrPhotos.slice().sort(compareComments);
+        break;
+    }
+
+    window.gallery.render(newPhotos);
+  });
+
+  var init = function (arrPhotos) {
+    filters.classList.remove('img-filters--inactive');
+    filters.querySelector('.img-filters__form').addEventListener('click', function (evt) {
+      if (evt.target.classList.contains('img-filters__button')) {
+        setActiveButton(evt.target);
+        applyFilter(arrPhotos, evt.target.id);
+      }
+    });
+  };
+
+  window.filters = {init: init};
+})();
